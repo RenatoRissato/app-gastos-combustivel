fcc3d19f9a20bb0c37c130b9f35116bcc3fae108
Renato Rissato <renatorissatodasilva55@gmail.com>
2025-05-22 17:44:00 -0300

    APP Android desenvolvido na faculdade.

---
+++ app/src/main/java/com/example/gastoscombustivel/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.gastoscombustivel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.example.gastoscombustivel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonCalcula.setOnClickListener(this)
+        }
+        override fun onClick(view: View){
+        if(view.id == R.id.button_calcula){
+            btCalcula()
+        }
+    }
+    fun btCalcula(){
+    try{
+        val distancia = binding.editDistancia.text.toString().toInt()
+        val preco = binding.editPreco.text.toString().toFloat()
+        val autonomia = binding.editAutonomia.text.toString().toFloat()
+
+        val gasto = (distancia / autonomia) * preco
+
+        binding.gasto.text = String.format("R$ %.2f", gasto)
+
+    }catch (e:Exception){
+        Toast.makeText(this, "Favor informar todos os valores!", Toast.LENGTH_SHORT).show()
+    }
+    }
+}
